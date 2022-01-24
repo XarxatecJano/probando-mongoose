53d866b8d3cefaa3a9211381150250995c942420
Jano Soler <jano.soler@xarxatecactiva.com>
2022-01-24 12:18:45 +0100

    mongoose funcionando

--- controller.js
+++ controller.js
@@ -1,5 +1,27 @@
 const path = require("path");
+const mongoose = require("mongoose");
+const User = require("./model/User");
+const connection = require("./config/connection");
+
+
 
 exports.sendLandingPage = (req,res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
+}
+
+exports.getNewUserPage = (req,res) => {
+    res.sendFile(path.join(__dirname, "public", "newUser.html"));
+}
+
+exports.insertNewUser = async (req, res) => {
+    const result = connection();
+    const date = new Date();
+    const user = new User({username:req.body.username, hashCode:req.body.password, salt:"", gender: "M"});
+    
+    user.newUser();
+    user.diHola();
+    user.password = "nuevoPassword";
+    user.save();
+    //User.newUser(req.body.username, req.body.password, "", date, "admin");
+    res.send("usuario grabado con éxito");
 }
